902b90905c393d0aeef3a9518997cc18b83f3c5d
akiyoshi_okumura <akiyoshia.m.1231@gmail.com>
2019-09-25 18:28:19 +0900

    add active_admin

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ActiveAdminApp
+  class Application < Rails::Application
+    
+    config.load_defaults 6.0
+    config.i18n.default_locale = :ja
+
+  end
+end
